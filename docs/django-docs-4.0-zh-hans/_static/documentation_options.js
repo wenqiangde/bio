9595129681749812ba1fa33840187da54e34b7ce
wenqiangde <andmaster@163.com>
2022-01-14 17:03:47 +0800

    添加新文档

---
+++ docs/django-docs-4.0-zh-hans/_static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '4.0.2.dev20220112064530',
+    LANGUAGE: 'zh_Hans',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+};
